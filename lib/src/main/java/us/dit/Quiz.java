f417a03e2b6474745ee29048be101e718beb6a7f
dit <dit@cdc317304>
2026-02-25 15:21:50 +0100

    My first IS quiz

---
+++ lib/src/main/java/us/dit/Quiz.java
@@ -0,0 +1,22 @@
+//uvus:BRL2259
+//pass:434
+
+package us.dit;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+        int[] numeros = {4, 9, 2, 7};
+        System.out.println(maximo(numeros));
+    }
+
+    public static int maximo(int[] array) {
+        int max = array[0];
+        for (int n : array) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        return max;
+    }
+}
